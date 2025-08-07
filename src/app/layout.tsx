4113a7b82c9ee2d185125f41165bbd6ad2dacbe5
Makena-WB <makenawahu@gmail.com>
2025-08-07 15:56:23 +0300

    Add Providers component to wrap Navbar and children in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/components/navbar";
+import { Providers } from "./providers";
 import "./globals.css";
 
 
@@ -6,9 +7,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className="p-4">
-        <Navbar />
-        {children}
-      </body>
+        <Providers>
+          <Navbar />
+          {children}
+        </Providers>
+        </body>
     </html>
   );
 }
